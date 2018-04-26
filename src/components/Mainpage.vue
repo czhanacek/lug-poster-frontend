eb0365982eff24b952c36ae782c27c711ba5a3f0
Charlie Hanacek <charlie@czhanacek.com>
2018-04-26 12:16:36 -0700

    initial commit

---
+++ src/components/Mainpage.vue
@@ -0,0 +1,59 @@
+
+
+<template>
+    <div>
+        <h3>Title</h3>
+        <input v-model.trim="name" placeholder="Title">
+        <hr>
+        <h4>Date</h4>
+        <input v-model.trim="date" placeholder="Date">
+        <hr>
+        <h4>Time</h4>
+        <input v-model.trim="time" placeholder="Time">
+        <hr>
+        <h4>Location</h4>
+        <input v-model.trim="location" placeholder="Sloan 169">
+        <hr>
+        <h4>Description</h4>
+        <textarea v-model.trim="description" placeholder="Description"></textarea>
+        <hr>
+        <input type="checkbox" id="pizza-checkbox" v-model="pizza">
+        <label for="pizza-checkbox">Pizza provided at event</label>
+        <br />
+        
+    </div>
+</template>
+
+
+
+<script>
+
+
+
+
+module.exports = {
+    data: function () {
+        return {
+            name: "",
+            date: "",
+            time: "",
+            location: "",
+            description: "",
+            pizza: false,
+            photo_url: "",
+            facebook_url: "",
+            gcal_url: "",
+            website_url: ""
+        }
+    },
+
+    mounted: function () {
+        
+        
+    }
+        
+}
+</script>
+
+
+
---
+++ src/main.js
@@ -0,0 +1,35 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+//import App from "./App.vue"
+import router from './router'
+
+import Mainpage from "./components/Mainpage.vue"
+import EventBus from "./event-bus.js"
+
+Vue.component("mainpage", Mainpage);
+
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  data () {
+    return {
+      currentView: 'mainpage'
+    }
+  },
+  http: {
+    root: "lugposter.wsu.party/" 
+  },
+  ready: function () {
+  
+  },
+  methods: {
+    changeView: function(newView) {
+      this.currentView = newView;
+    }
+  }
+})
